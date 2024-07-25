89dda292a7c74e147475c55815f3876ad0042ad7
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-25 21:54:28 +0700

    module, controller, dan http method

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('/api/users')
+export class UserController {
+    @Post()
+    post(): string {
+        return 'POST';
+    }
+
+    @Get('/sample')
+    get(): string {
+        return 'GET';
+    } 
+}
