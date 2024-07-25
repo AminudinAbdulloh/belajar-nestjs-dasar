cb7a2e302ab1d948dc8801ce429ec43abef41b55
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-25 22:30:01 +0700

    http request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,36 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
     @Post()
-    post(): string {
-        return 'POST';
+    createUser(): string {
+        return 'POST User';
     }
 
     @Get('/sample')
-    get(): string {
-        return 'GET';
+    getUser(): string {
+        return 'GET User';
     } 
+
+    // Untuk express.Request
+    // @Get('/sample/:id')
+    // getUserById(@Req() request: Request): string {
+    //     return `GET User ID: ${request.params.id}`;
+    // }
+
+    // Untuk req.params.id?
+    @Get('sample/:id')
+    getUserById(@Param("id") id): string {
+        return `GET ${id}`;
+    }
+
+    // Untuk req.query.key?
+    @Get('/hello')
+    sayHello(
+        @Query("first_name") first_name: string,
+        @Query("last_name") last_name: string
+    ): string {
+        return `Hello ${first_name} ${last_name}`;
+    }
 }
