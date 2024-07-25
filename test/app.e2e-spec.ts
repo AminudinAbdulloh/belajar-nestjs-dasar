42c8ed451701597ea330db23dc071bbe702bcdb5
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-26 02:04:22 +0700

    integration test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,16 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('should can say hello', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/api/users/hello')
+      .query({
+        first_name: 'Aminudin',
+        last_name: 'Abdulloh'
+      });
+
+      expect(result.status).toBe(200);
+      expect(result.text).toBe('Hello Aminudin Abdulloh');
+  })
 });
