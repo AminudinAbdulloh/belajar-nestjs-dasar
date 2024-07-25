cf304a5eed779bf77ec3d8a73c9ec637cc091682
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-26 01:54:46 +0700

    unit test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,12 +8,27 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      imports: [],
+      providers: []
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Aminudin', 'Abdulloh');
+    expect(response).toBe('Hello Aminudin Abdulloh');
   });
+
+  // Menggunakan Mock Object untuk response yang menggunakan express.Response
+  it('should can view hello',async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Amin', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      title: 'Template Engine',
+      name: 'Amin'
+    })
+  })
 });
