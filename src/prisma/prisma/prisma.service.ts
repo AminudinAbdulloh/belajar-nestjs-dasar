704dcd03dc61d17ec0c9d889c39573f7f68e36cf
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-07-27 17:01:41 +0700

    Lifecycle Event

--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
         super();
         console.log('Create Prisma Service');
+    }
+
+    
+    onModuleInit() {
+        console.log('Connect Prisma');
+        this.$connect();
+    }
+
+    onModuleDestroy() {
+        console.log('Disconnect Prisma');
+        this.$disconnect();
     }
 }
